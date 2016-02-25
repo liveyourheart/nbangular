408b1a9e9eecd4b4812234cdc32d1153ea53bd16
Matt McHughes <liveyourheart@gmail.com>
2016-02-25 15:42:42 -0600

    teamStat api integration test

--- server/api/teamStat/teamStat.integration.js
+++ server/api/teamStat/teamStat.integration.js
@@ -8,11 +8,11 @@ var newTeamStat;
 describe('TeamStat API:', function() {
 
   describe('GET /api/teamStats/:id', function() {
-    var teamStat;
+    var teamStat, ranks, info;
 
     beforeEach(function(done) {
       request(app)
-        .get('/api/teamStats/' + newTeamStat._id)
+        .get('/api/teamStats/' + 1610612760)
         .expect(200)
         .expect('Content-Type', /json/)
         .end((err, res) => {
@@ -20,6 +20,8 @@ describe('TeamStat API:', function() {
             return done(err);
           }
           teamStat = res.body;
+          ranks = res.body.teamSeasonRanks[0];
+          info = res.body.teamInfoCommon[0];
           done();
         });
     });
@@ -29,8 +31,27 @@ describe('TeamStat API:', function() {
     });
 
     it('should respond with the requested teamStat', function() {
-      teamStat.name.should.equal('New TeamStat');
-      teamStat.info.should.equal('This is the brand new teamStat!!!');
+      teamStat.should.not.equal(null);
+      teamStat.teamSeasonRanks.should.not.equal(null);
+      teamStat.teamInfoCommon.should.not.equal(null);
+    });
+
+    it('should respond with the requested team season ranks', function() {
+      ranks.should.not.equal(null);
+      ranks.leagueId.should.equal('00');
+      ranks.seasonId.should.equal('22015');
+      ranks.teamId.should.equal(1610612760)
+    });
+
+    it('should respond with the requested team Info', function() {
+      info.should.not.equal(null);
+      info.seasonYear.should.equal('2015-16');
+      info.teamCity.should.equal('Oklahoma City');
+      info.teamName.should.equal('Thunder');
+      info.teamAbbreviation.should.equal('OKC');
+      info.teamConference.should.equal('West');
+      info.teamDivision.should.equal('Northwest');
+
     });
 
   });
