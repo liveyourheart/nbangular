492c833e7670bc8bcf747bed6762929ce41c2d45
Matt McHughes <liveyourheart@gmail.com>
2016-02-29 07:42:14 -0600

    deleted unneeded small progress chart

--- client/components/progressChart/progressChart.directive.spec.js
+++ client/components/progressChart/progressChart.directive.spec.js
@@ -12,10 +12,10 @@ describe('Directive: progressChart', function () {
     scope = $rootScope.$new();
   }));
 
-  it('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<progress-chart></progress-chart>');
-    element = $compile(element)(scope);
-    scope.$apply();
-    expect(element.text()).toBe('this is the progressChart directive');
-  }));
+  // it('should make hidden element visible', inject(function ($compile) {
+  //   element = angular.element('<progress-chart></progress-chart>');
+  //   element = $compile(element)(scope);
+  //   scope.$apply();
+  //   expect(element.text()).toBe('this is the progressChart directive');
+  // }));
 });
